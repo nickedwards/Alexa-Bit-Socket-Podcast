d2150c354bea93642959dc02dc4a44045491e86f
Nick Edwards <nick@nick-edwards.co.uk>
2018-06-26 19:25:30 +0100

    add welcome back message

--- src/handlers/launchRequestHandler.js
+++ src/handlers/launchRequestHandler.js
@@ -1,16 +1,22 @@
 
 const copy = {
     welcomeMessage: 'Welcome to the Bit Socket Podcast. Which episode would you like to listen to?',
-    welcomeReprompt: 'You can say "play latest" to listen to our latest episode, or "list podcasts" to hear about other episodes. Say "help" for other options.'
+    welcomeReprompt: 'You can say "play latest" to listen to our latest episode, or "list podcasts" to hear about other episodes. Say "help" for other options.',
+    welcomeBackMessage: 'Welcome back. You were listening to ::episodeTitle::, you can continue listening by saying "resume", or ask to play a different episode. Which episode would you like to listen to?'
 };
 
 module.exports = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
-    handle(handlerInput) {
+    async handle(handlerInput) {
+        let attributes = await handlerInput.attributesManager.getPersistentAttributes();
+        let message = copy.welcomeMessage;
+        if (attributes.offsetInMilliseconds) {
+            message = copy.welcomeBackMessage.replace("::episodeTitle::", attributes.episodeTitle);
+        }
         return handlerInput.responseBuilder
-            .speak(copy.welcomeMessage)
+            .speak(message)
             .reprompt(copy.welcomeReprompt)
             .getResponse();
     }
